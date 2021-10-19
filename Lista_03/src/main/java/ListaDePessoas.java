044b7a8d06ab91d4f1b8c815eee2aa26aebaa5d1
Marcus Davi S. Queiros <marcusdavi.soar@gmail.com>
2021-10-19 17:19:27 -0300

    Create ListaDePessoas.java

---
+++ Lista_03/src/main/java/ListaDePessoas.java
@@ -0,0 +1,35 @@
+package lista_03;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonRootName("ListaDePessoas")
+public class ListaDePessoas implements Serializable {
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "Pessoa")
+    List<Pessoa> pessoas;
+
+    public ListaDePessoas(){
+        super();
+        this.pessoas = new ArrayList<>();
+    }
+
+    public ListaDePessoas(Pessoa ...pessoas){
+        super();
+        this.pessoas = new ArrayList<>();
+
+        for (Pessoa p:pessoas) {
+            this.pessoas.add(p);
+        }
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+}
